cdc1781099a5664d37eec60bca99a85c41ff1f77
0x1zen <rajdubalwork@gmail.com>
2024-12-08 18:37:51 +0530

    mod:corrected bubble sort

--- 3.Sorting-Techniques/2_bubbleSort.cpp
+++ 3.Sorting-Techniques/2_bubbleSort.cpp
@@ -7,10 +7,10 @@ void swap(int &x,int &y){
 }
 void sort(int arr[6],int size){
     // push the max to the last by adjuscent swaps
-    for(int i=0;i<size-1;i++){
-        for(int j=i+1;j<size;j++){
-            if(arr[i]>arr[j]){
-                swap(arr[i],arr[j]);
+    for(int i=size-1;i>=1;i--){
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
             }
         }
     }
