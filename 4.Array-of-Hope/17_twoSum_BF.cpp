56b9fc2d57e4cdaac5175cc423b51407d880e1a8
Raj Dubal <126544333+0x1zen@users.noreply.github.com>
2025-01-03 05:05:23 +0000

    Two Sum Brute

---
+++ 4.Array-of-Hope/17_twoSum_BF.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> twoSum(vector<int> &nums, int target) {
+  vector<int> ans;
+  for (int i = 0; i < nums.size() - 1; i++) {
+    for (int j = i + 1; j < nums.size(); j++) {
+      if (nums[i] + nums[j] == target) {
+        ans.push_back(i);
+        ans.push_back(j);
+        return ans;
+      }
+    }
+  }
+  return ans;
+}
+int main() {
+  vector<int> nums = {2, 7, 11, 15};
+  int target = 9;
+  vector<int> ans = twoSum(nums, target);
+  for (int value : ans) {
+    cout << value << " ";
+  }
+}
