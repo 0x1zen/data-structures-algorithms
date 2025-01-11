42efd4a5acf5bcdb0f12099229e69e8d535786d1
Raj Dubal <126544333+0x1zen@users.noreply.github.com>
2025-01-11 15:19:34 +0530

    53.Max Subarray Sum Brute Force Solution

---
+++ 4.Array-of-Hope/20_maxSubarraySum_BF.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<vector>
+#include<climits>
+using namespace std;
+int maxSubArray(vector<int>& nums) {
+    int maxSum=INT_MIN;
+    for(int i=0;i<nums.size();i++){
+        int sum=0;
+        for(int j=i;j<nums.size();j++){
+            sum+=nums[j];
+            maxSum=max(sum,maxSum);
+        }
+    }
+    return maxSum;
+}
+int main() {
+    vector<int>nums={-2,1,-3,4,-1,2,1,-5,4};
+    cout<<maxSubArray(nums);
+
+    return 0;
+}
